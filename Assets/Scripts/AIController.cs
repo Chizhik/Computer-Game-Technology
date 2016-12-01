3a49e2298c0fc16f0d611ef185d1b181783b845d
Alisher Tortay <t.b.alisher@gmail.com>
2016-12-02 00:07:42 +0900

    implomented simple and weird AI behavior for board without obstacles

---
+++ Assets/Scripts/AIController.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AIController : MonoBehaviour {
+
+    public float speed;
+    public float coef;
+    public GameObject target; //pursue
+    public GameObject enemy; //flee
+
+    private Rigidbody myrb;
+
+	// Use this for initialization
+	void Start () {
+
+        myrb = GetComponent<Rigidbody>();
+	
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+
+        Vector3 v = myrb.velocity;
+        Vector3 targetDir = target.transform.position - myrb.position;
+        targetDir = coef * targetDir / targetDir.sqrMagnitude;
+        Vector3 enemyDir = enemy.transform.position - myrb.position;
+        enemyDir = coef * enemyDir / enemyDir.sqrMagnitude;
+        Vector3 movement = targetDir - enemyDir - v;
+        myrb.AddForce(movement * speed);
+
+	
+	}
+}
