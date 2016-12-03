01756039af461eefdfd815749249e93a056bf73d
Alisher Tortay <t.b.alisher@gmail.com>
2016-12-03 18:00:03 +0900

    implemented estimation of future position

--- Assets/Scripts/AIController.cs
+++ Assets/Scripts/AIController.cs
@@ -8,6 +8,8 @@ public class AIController : MonoBehaviour {
     public GameObject enemy; //flee
 
     private Rigidbody myrb;
+    private Rigidbody tgRigid;
+    private Rigidbody enRigid;
     private Vector3 northWall;
     private Vector3 southWall;
     private Vector3 westWall;
@@ -17,6 +19,8 @@ public class AIController : MonoBehaviour {
 	void Start () {
 
         myrb = GetComponent<Rigidbody>();
+        tgRigid = target.GetComponent<Rigidbody>();
+        enRigid = enemy.GetComponent<Rigidbody>();
         northWall = new Vector3(0, 0, 10);
         southWall = new Vector3(0, 0, -10);
         westWall = new Vector3(-10, 0, 0);
@@ -28,10 +32,8 @@ public class AIController : MonoBehaviour {
 	void FixedUpdate () {
 
         Vector3 v = myrb.velocity;
-        Vector3 seekVel = target.transform.position - myrb.position;
-        seekVel = seekVel / seekVel.sqrMagnitude;
-        Vector3 fleeVel = myrb.position - enemy.transform.position;
-        fleeVel = fleeVel / fleeVel.sqrMagnitude;
+        Vector3 seekVel = pursuit(tgRigid.position, tgRigid.velocity, myrb.position);
+        Vector3 fleeVel = evade(enRigid.position, enRigid.velocity, myrb.position);
 
         Vector3 northWallDir = new Vector3(0, 0, myrb.position.z - northWall.z);
         Vector3 southWallDir = new Vector3(0, 0, myrb.position.z - southWall.z);
@@ -48,7 +50,30 @@ public class AIController : MonoBehaviour {
         //enemyDir = enemyDir / enemyDir.sqrMagnitude;
         //Vector3 movement = (targetDir - enemyDir).normalized;
         myrb.AddForce(myrb.mass * steering / Time.fixedDeltaTime);
-
-	
 	}
+
+    private Vector3 pursuit(Vector3 tgPos, Vector3 tgVel,  Vector3 mypos)
+    {
+        float dist = (tgPos - mypos).magnitude;
+        //float T = dist / speed;
+        float T = Time.fixedDeltaTime * dist / speed;
+        Vector3 futurePosition = tgPos + tgVel * T;
+        Vector3 seekVel = futurePosition - mypos;
+        //Vector3 seekVel = tgPos - mypos;
+        seekVel = seekVel / seekVel.sqrMagnitude;
+        return seekVel;
+    }
+
+    private Vector3 evade(Vector3 enPos, Vector3 enVel, Vector3 mypos)
+    {
+        float dist = (enPos - mypos).magnitude;
+        //float T = dist / speed;
+        float T = Time.fixedDeltaTime * dist / speed;
+        Vector3 futurePosition = enPos + enVel * T;
+        Vector3 fleeVel = mypos - futurePosition;
+        //Vector3 fleeVel = mypos - enPos;
+        fleeVel = fleeVel / fleeVel.sqrMagnitude;
+        return fleeVel;
+    }
+
 }
